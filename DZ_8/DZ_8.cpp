93f33dac114238a03abd2a8e48498dd6f8bbb4f2
DaniiLord <daniilsveckarev07@gmail.com>
2025-03-02 20:39:26 +0200

    Добавьте файлы проекта.

---
+++ DZ_8/DZ_8.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string S = "hello world. hello step. hello it.";
+    string S0 = "hello";
+    cout << S << endl;
+
+    size_t pos = S.find(S0);
+
+    if (pos < S.length()) {
+        S.erase(pos, S0.length());
+    }
+    cout << S << endl;
+
+    return 0;
+}
